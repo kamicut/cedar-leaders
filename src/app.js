21cd8f7ea3a84b07d23eccc4ae9ec230962bc819
Marc Farra <marcfarra@gmail.com>
2016-12-04 23:56:18 -0500

    Add map view

--- src/app.js
+++ src/app.js
@@ -1,1 +1,32 @@
-console.log('hello world!');
+const choo = require('choo');
+const html = require('choo/html');
+
+const Map = require('./map')();
+
+const app = choo();
+
+app.model({
+  state: {
+    coords: [33.888629, 35.495479]
+  },
+  reducers: {
+    setCoords: (data, state) => {
+      return { coords: data }
+    }
+  }
+});
+
+const View = (state, prev, send) => {
+  return html`
+  <main>
+    ${Map(state.coords)}
+  </main>
+  `
+}
+
+app.router((route) => [ 
+	route('/', View)
+])
+
+const tree = app.start();
+document.getElementById('map').appendChild(tree);
